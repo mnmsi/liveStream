9773ba8463410301ba3f0c16ddfde17ec54a8620
saifuliotait <saiful.iotait@gmail.com>
2024-03-26 16:41:14 +0600

    log update

--- app/Http/Traits/CommonTrait.php
+++ app/Http/Traits/CommonTrait.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Traits;
 
+use Carbon\Carbon;
+
 trait CommonTrait
 {
     public function getBandwidth($logDir): array
     {
+        $logDir = 'C:\Users\saifu\Documents\ipl_bw.log';
+
         // Check if the log file exists
         if (!file_exists($logDir)) {
             return [
@@ -21,13 +25,28 @@ trait CommonTrait
         $incomingBandwidth = 0;
         $outgoingBandwidth = 0;
 
+        // Calculate start time as current time
+        $startTime = Carbon::now();
+
+        // Calculate end time by subtracting 5 seconds from the start time
+        $endTime = $startTime->copy()->subSeconds(5);
+
         // Read the log file line by line
         while ($line = fgets($logFile)) {
             // Parse the line based on the log format
             $logParts = explode(' ', $line);
 
-            $incomingBandwidth     += !empty($logParts[24]) ? intval($logParts[24]) : 0;
-            $outgoingBandwidth     += !empty($logParts[25]) ? intval($logParts[25]) : 0;
+            // Get the timestamp from the log line
+            $logTimestamp = str_replace('[', '', $logParts[3]);
+
+            // Parse the time using Carbon
+            $logDateTime = Carbon::createFromFormat('d/M/Y:H:i:s', $logTimestamp);
+
+            // Check if the log line falls within the specified time frame
+            if ($logDateTime->between($endTime, $startTime)) {
+                $incomingBandwidth += !empty($logParts[24]) ? intval($logParts[24]) : 0;
+                $outgoingBandwidth += !empty($logParts[25]) ? intval($logParts[25]) : 0;
+            }
         }
 
         // Close the log file
